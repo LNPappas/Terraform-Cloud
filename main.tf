cefa4cea52c1eb53e76a02378519f04dfa0abdd1
LNPappas <lpappas@gmail.com>
2021-02-05 11:24:43 -0800

    added provider

--- main.tf
+++ main.tf
@@ -1,3 +1,11 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "3.55.0"
+    }
+  }
+}
 resource "google_storage_bucket" "gcs_bucket" {
   name = "test-bucket-terraform-cloud-pipeline"
 }
