4ad5d0dc351c2816c5f007dc52edec897c3b9d97
LNPappas <lpappas@gmail.com>
2021-02-08 09:56:09 -0800

    removed buckets

--- main.tf
+++ main.tf
@@ -10,12 +10,13 @@ terraform {
 provider "google" {
   project = "demotfcloud"
   region  = "us-central1"
+  
 }
 
-resource "google_storage_bucket" "gcs_bucket" {
-  name = "1st-test-bucket-terraform-cloud-pipeline"
-}
+# resource "google_storage_bucket" "gcs_bucket" {
+#   name = "1st-test-bucket-terraform-cloud-pipeline"
+# }
 
-resource "google_storage_bucket" "gcs_bucket_2" {
-  name = "2nd-test-bucket-terraform-cloud-pipeline"
-}
+# resource "google_storage_bucket" "gcs_bucket_2" {
+#   name = "2nd-test-bucket-terraform-cloud-pipeline"
+# }
