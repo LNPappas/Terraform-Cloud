0312e25d43c4e35b4256212c6f0ac49202fd9c83
LNPappas <LPappas@gmail.com>
2021-05-03 13:48:09 -0700

    uncommented repo/workspace c

--- github.tf
+++ github.tf
@@ -80,84 +80,84 @@ resource "github_repository_file" "variables" {
   }
 }
 
-// resource "github_repository" "repo_c" {
-//   name = "repo_c"
-//   auto_init = true
-//   vulnerability_alerts = true
-// }
+resource "github_repository" "repo_c" {
+  name = "repo_c"
+  auto_init = true
+  vulnerability_alerts = true
+}
 
-// resource "github_repository_file" "c_tfvars" {
-//   repository          = github_repository.repo_c.name
-//   branch              = "main"
-//   file                = "terraform.auto.tfvars"
-//   content             = file("terraform.auto.tfvars")
-//   commit_message      = "initial commit"
-//   commit_author       = "Lauren Pappas"
-//   commit_email        = "LPappas@gmail.com"
-//   overwrite_on_create = true
+resource "github_repository_file" "c_tfvars" {
+  repository          = github_repository.repo_c.name
+  branch              = "main"
+  file                = "terraform.auto.tfvars"
+  content             = file("terraform.auto.tfvars")
+  commit_message      = "initial commit"
+  commit_author       = "Lauren Pappas"
+  commit_email        = "LPappas@gmail.com"
+  overwrite_on_create = true
 
-//   lifecycle {
-//     ignore_changes = [
-//       commit_email,
-//       commit_author,
-//       branch
-//     ]
-//   }
-// }
+  lifecycle {
+    ignore_changes = [
+      commit_email,
+      commit_author,
+      branch
+    ]
+  }
+}
 
-// resource "github_repository_file" "main" {
-//   repository          = github_repository.repo_c.name
-//   branch              = "main"
-//   file                = "main.tf"
-//   content             = file("main.tf")
-//   commit_message      = "initial commit"
-//   commit_author       = "Lauren Pappas"
-//   commit_email        = "LPappas@gmail.com"
-//   overwrite_on_create = true
+resource "github_repository_file" "main_c" {
+  repository          = github_repository.repo_c.name
+  branch              = "main"
+  file                = "main.tf"
+  content             = file("main.tf")
+  commit_message      = "initial commit"
+  commit_author       = "Lauren Pappas"
+  commit_email        = "LPappas@gmail.com"
+  overwrite_on_create = true
 
-//   lifecycle {
-//     ignore_changes = [
-//       commit_email,
-//       commit_author,
-//       branch
-//     ]
-//   }
-// }
+  lifecycle {
+    ignore_changes = [
+      commit_email,
+      commit_author,
+      branch
+    ]
+  }
+}
 
-// resource "github_repository_file" "providers" {
-//   repository          = github_repository.repo_c.name
-//   branch              = "main"
-//   file                = "providers.tf"
-//   content             = file("providers.tf")
-//   commit_message      = "initial commit"
-//   commit_author       = "Lauren Pappas"
-//   commit_email        = "LPappas@gmail.com"
-//   overwrite_on_create = true
+resource "github_repository_file" "providers_c" {
+  repository          = github_repository.repo_c.name
+  branch              = "main"
+  file                = "providers.tf"
+  content             = file("providers.tf")
+  commit_message      = "initial commit"
+  commit_author       = "Lauren Pappas"
+  commit_email        = "LPappas@gmail.com"
+  overwrite_on_create = true
 
-//   lifecycle {
-//     ignore_changes = [
-//       commit_email,
-//       commit_author,
-//       branch
-//     ]
-//   }
-// }
+  lifecycle {
+    ignore_changes = [
+      commit_email,
+      commit_author,
+      branch
+    ]
+  }
+}
 
-// resource "github_repository_file" "variables" {
-//   repository          = github_repository.repo_c.name
-//   branch              = "main"
-//   file                = "variables.tf"
-//   content             = file("variables.tf")
-//   commit_message      = "initial commit"
-//   commit_author       = "Lauren Pappas"
-//   commit_email        = "LPappas@gmail.com"
-//   overwrite_on_create = true
+resource "github_repository_file" "variables_c" {
+  repository          = github_repository.repo_c.name
+  branch              = "main"
+  file                = "variables.tf"
+  content             = file("variables.tf")
+  commit_message      = "initial commit"
+  commit_author       = "Lauren Pappas"
+  commit_email        = "LPappas@gmail.com"
+  overwrite_on_create = true
 
-//   lifecycle {
-//     ignore_changes = [
-//       commit_email,
-//       commit_author,
-//       branch
-//     ]
-//   }
-// }
+  lifecycle {
+    ignore_changes = [
+      commit_email,
+      commit_author,
+      branch
+    ]
+  }
+}
--- tfe.tf
+++ tfe.tf
@@ -99,94 +99,100 @@ resource "tfe_run_trigger" "trigger_b" {
   sourceable_id = var.workspace_trigger_id
 }
 
-// resource "tfe_workspace" "workspace_c" {
-//   name              = "workspace_c"
-//   organization      = "db_test"
-//   terraform_version = "0.14.5"
-//   queue_all_runs    = false
+resource "tfe_workspace" "workspace_c" {
+  name              = "workspace_c"
+  organization      = "db_test"
+  terraform_version = "0.14.5"
+  queue_all_runs    = false
 
-//   vcs_repo {
-//     identifier     = "LNPappas/repo_c"
-//     branch         = "main"
-//     oauth_token_id = "ot-2NQF7eb6F3E4s21g"
-//   }
-// }
+  vcs_repo {
+    identifier     = "LNPappas/repo_c"
+    branch         = "main"
+    oauth_token_id = "ot-2NQF7eb6F3E4s21g"
+  }
+}
 
-// resource "tfe_variable" "c_tfvars_plan" {
-//   key          = "TF_CLI_ARGS_plan"
-//   value        = "terraform.auto.tfvars"
-//   category     = "terraform"
-//   workspace_id = tfe_workspace.workspace_c.id
-// }
-
-// resource "tfe_variable" "bucket_1" {
-//   key          = "bucket_1"
-//   value        = "${var.bucket_1}-${tfe_workspace.workspace_c.name}"
-//   category     = "terraform"
-//   workspace_id = tfe_workspace.workspace_c.id
-// }
+resource "tfe_variable" "bucket_1" {
+  key          = "bucket_1"
+  value        = "${var.bucket_1}-${tfe_workspace.workspace_c.name}"
+  category     = "terraform"
+  workspace_id = tfe_workspace.workspace_c.id
+}
 
 
-// resource "tfe_variable" "bucket_2" {
-//   key          = "bucket_2"
-//   value        = "${var.bucket_2}-${tfe_workspace.workspace_c.name}"
-//   category     = "terraform"
-//   workspace_id = tfe_workspace.workspace_c.id
-// }
+resource "tfe_variable" "bucket_2" {
+  key          = "bucket_2"
+  value        = "${var.bucket_2}-${tfe_workspace.workspace_c.name}"
+  category     = "terraform"
+  workspace_id = tfe_workspace.workspace_c.id
+}
 
 
-// resource "tfe_variable" "project_name" {
-//   key          = "project_name"
-//   value        = var.project_name
-//   category     = "terraform"
-//   workspace_id = tfe_workspace.workspace_c.id
-// }
+resource "tfe_variable" "project_name" {
+  key          = "project_name"
+  value        = var.project_name
+  category     = "terraform"
+  workspace_id = tfe_workspace.workspace_c.id
+}
 
 
-// resource "tfe_variable" "region" {
-//   key          = "region"
-//   value        = var.region
-//   category     = "terraform"
-//   workspace_id = tfe_workspace.workspace_c.id
-// }
+resource "tfe_variable" "region" {
+  key          = "region"
+  value        = var.region
+  category     = "terraform"
+  workspace_id = tfe_workspace.workspace_c.id
+}
 
-// resource "tfe_variable" "github_token" {
-//   key          = "github_token"
-//   value        = var.github_token
-//   category     = "terraform"
-//   sensitive    = true
-//   workspace_id = tfe_workspace.workspace_c.id
-// }
+resource "tfe_variable" "github_token" {
+  key          = "github_token"
+  value        = var.github_token
+  category     = "terraform"
+  sensitive    = true
+  workspace_id = tfe_workspace.workspace_c.id
+}
 
-// resource "tfe_variable" "github_token_env" {
-//   key          = "github_token"
-//   value        = var.github_token
-//   category     = "env"
-//   sensitive    = true
-//   workspace_id = tfe_workspace.workspace_c.id
-// }
+resource "tfe_variable" "github_token_env" {
+  key          = "github_token"
+  value        = var.github_token
+  category     = "env"
+  sensitive    = true
+  workspace_id = tfe_workspace.workspace_c.id
+}
 
-// resource "tfe_variable" "github_token" {
-//   key          = "github_token"
-//   value        = var.github_token
-//   category     = "env"
-//   sensitive    = true
-//   workspace_id = tfe_workspace.workspace_c.id
-// }
+resource "tfe_variable" "github_token" {
+  key          = "github_token"
+  value        = var.github_token
+  category     = "env"
+  sensitive    = true
+  workspace_id = tfe_workspace.workspace_c.id
+}
 
 
-// resource "tfe_variable" "github_token" {
-//   key          = "github_token"
-//   value        = var.github_token
-//   category     = "env"
-//   sensitive    = true
-//   workspace_id = tfe_workspace.workspace_c.id
-// }
+resource "tfe_variable" "github_token" {
+  key          = "github_token"
+  value        = var.github_token
+  category     = "env"
+  sensitive    = true
+  workspace_id = tfe_workspace.workspace_c.id
+}
 
-// resource "tfe_variable" "google_credentials" {
-//   key          = "google_credentials"
-//   value        = var.google_credentials
-//   category     = "terraform"
-//   sensitive    = true
-//   workspace_id = tfe_workspace.workspace_c.id
-// }
+resource "tfe_variable" "google_credentials" {
+  key          = "google_credentials"
+  value        = var.google_credentials
+  category     = "terraform"
+  sensitive    = true
+  workspace_id = tfe_workspace.workspace_c.id
+}
+
+resource "tfe_variable" "workspace_trigger_id_c" {
+  key          = "workspace_trigger_id"
+  value        = var.workspace_trigger_id
+  category     = "terraform"
+  sensitive    = false
+  workspace_id = tfe_workspace.workspace_c.id
+}
+
+resource "tfe_run_trigger" "trigger_c" {
+  workspace_id  = tfe_workspace.workspace_c.id
+  sourceable_id = var.workspace_trigger_id
+}
