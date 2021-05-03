4f2e3ba1f627eddadfcebf9dda18ecf4f28901db
LNPappas <LPappas@gmail.com>
2021-05-03 10:54:50 -0700

    testing cloud resources

--- github.tf
+++ github.tf
@@ -1,48 +1,11 @@
-// resource "time_sleep" "wait_30_seconds" {
-//   depends_on = [github_repository.repo_b]
-
-//   create_duration = "30s"
-// }
-
-// # This resource will create (at least) 30 seconds after null_resource.previous
-// resource "null_resource" "next" {
-//   depends_on = [time_sleep.wait_30_seconds]
-// }
-
-resource "github_repository" "repo_b" {
-  name                 = "repo_b"
-  auto_init            = true
-  vulnerability_alerts = true
-}
-
-// resource "github_repository" "repo_c" {
-//   name = "repo_c"
-//   auto_init = true
+// resource "github_repository" "repo_b" {
+//   name                 = "repo_b"
+//   auto_init            = true
 //   vulnerability_alerts = true
 // }
 
-resource "github_repository_file" "b_tfvars" {
-  //   depends_on          = [time_sleep.wait_30_seconds]
-  repository          = github_repository.repo_b.name
-  branch              = "main"
-  file                = "terraform.auto.tfvars"
-  content             = file("terraform.auto.tfvars")
-  commit_message      = "initial commit"
-  commit_author       = "Lauren Pappas"
-  commit_email        = "LPappas@gmail.com"
-  overwrite_on_create = true
-
-  lifecycle {
-    ignore_changes = [
-      commit_email,
-      commit_author,
-      branch
-    ]
-  }
-}
-
-// resource "github_repository_file" "c_tfvars" {
-//   repository          = github_repository.repo_c.name
+// resource "github_repository_file" "b_tfvars" {
+//   repository          = github_repository.repo_b.name
 //   branch              = "main"
 //   file                = "terraform.auto.tfvars"
 //   content             = file("terraform.auto.tfvars")
@@ -59,3 +22,66 @@ resource "github_repository_file" "b_tfvars" {
 //     ]
 //   }
 // }
+
+// resource "github_repository_file" "main" {
+//   repository          = github_repository.repo_b.name
+//   branch              = "main"
+//   file                = "main.tf"
+//   content             = file("main.tf")
+//   commit_message      = "initial commit"
+//   commit_author       = "Lauren Pappas"
+//   commit_email        = "LPappas@gmail.com"
+//   overwrite_on_create = true
+
+//   lifecycle {
+//     ignore_changes = [
+//       commit_email,
+//       commit_author,
+//       branch
+//     ]
+//   }
+// }
+
+// // resource "github_repository" "repo_c" {
+// //   name = "repo_c"
+// //   auto_init = true
+// //   vulnerability_alerts = true
+// // }
+
+// // resource "github_repository_file" "c_tfvars" {
+// //   repository          = github_repository.repo_c.name
+// //   branch              = "main"
+// //   file                = "terraform.auto.tfvars"
+// //   content             = file("terraform.auto.tfvars")
+// //   commit_message      = "initial commit"
+// //   commit_author       = "Lauren Pappas"
+// //   commit_email        = "LPappas@gmail.com"
+// //   overwrite_on_create = true
+
+// //   lifecycle {
+// //     ignore_changes = [
+// //       commit_email,
+// //       commit_author,
+// //       branch
+// //     ]
+// //   }
+// // }
+
+// // resource "github_repository_file" "main" {
+// //   repository          = github_repository.repo_c.name
+// //   branch              = "main"
+// //   file                = "main.tf"
+// //   content             = file("main.tf")
+// //   commit_message      = "initial commit"
+// //   commit_author       = "Lauren Pappas"
+// //   commit_email        = "LPappas@gmail.com"
+// //   overwrite_on_create = true
+
+// //   lifecycle {
+// //     ignore_changes = [
+// //       commit_email,
+// //       commit_author,
+// //       branch
+// //     ]
+// //   }
+// // }
--- tfe.tf
+++ tfe.tf
@@ -1,85 +1,123 @@
-# Testing enviornment to use 1 workspace to spin up 2 others
-# workspace uses a runs trigger on workspaces b & c  when variables are set
+// # Testing enviornment to use 1 workspace to spin up 2 others
+// # workspace uses a runs trigger on workspaces b & c  when variables are set
 
-resource "tfe_workspace" "workspace_b" {
-  //   depends_on        = [github_repository_file.b_tfvars]
-  name              = "workspace_b"
-  organization      = "db_test"
-  terraform_version = "0.14.5"
-  queue_all_runs    = false
-
-  vcs_repo {
-    identifier     = "LNPappas/${github_repository.repo_b.name}"
-    branch         = "main"
-    oauth_token_id = "ot-2NQF7eb6F3E4s21g"
-  }
-}
-
-// resource "tfe_workspace" "workspace_c" {
-//   name              = "workspace_c"
+// resource "tfe_workspace" "workspace_b" {
+//   //   depends_on        = [github_repository_file.b_tfvars]
+//   name              = "workspace_b"
 //   organization      = "db_test"
 //   terraform_version = "0.14.5"
 //   queue_all_runs    = false
 
 //   vcs_repo {
-//     identifier     = "LNPappas/repo_c"
+//     identifier     = "LNPappas/${github_repository.repo_b.name}"
 //     branch         = "main"
 //     oauth_token_id = "ot-2NQF7eb6F3E4s21g"
 //   }
 // }
 
-
-resource "tfe_variable" "b_tfvars_plan" {
-  key          = "TF_CLI_ARGS_plan"
-  value        = file("terraform.auto.tfvars")
-  category     = "terraform"
-  workspace_id = tfe_workspace.workspace_b.id
-}
-
-// resource "tfe_variable" "c_tfvars_plan" {
+// resource "tfe_variable" "b_tfvars_plan" {
 //   key          = "TF_CLI_ARGS_plan"
-//   value        = "terraform.auto.tfvars"
+//   value        = file("terraform.auto.tfvars")
 //   category     = "terraform"
-//   workspace_id = tfe_workspace.workspace_c.id
+//   workspace_id = tfe_workspace.workspace_b.id
 // }
 
-resource "tfe_variable" "bucket_1" {
-  key          = "bucket_1"
-  value        = var.bucket_1
-  category     = "terraform"
-  workspace_id = tfe_workspace.workspace_b.id
-}
+// resource "tfe_variable" "bucket_1" {
+//   key          = "bucket_1"
+//   value        = "${var.bucket_1}+${tfe_workspace.workspace_b.id}"
+//   category     = "terraform"
+//   workspace_id = tfe_workspace.workspace_b.id
+// }
 
 
-resource "tfe_variable" "bucket_2" {
-  key          = "bucket_2"
-  value        = var.bucket_2
-  category     = "terraform"
-  workspace_id = tfe_workspace.workspace_b.id
-}
+// resource "tfe_variable" "bucket_2" {
+//   key          = "bucket_2"
+//   value        = "${var.bucket_2}+${tfe_workspace.workspace_b.id}"
+//   category     = "terraform"
+//   workspace_id = tfe_workspace.workspace_b.id
+// }
 
 
-resource "tfe_variable" "project_name" {
-  key          = "project_name"
-  value        = var.project_name
-  category     = "terraform"
-  workspace_id = tfe_workspace.workspace_b.id
-}
+// resource "tfe_variable" "project_name" {
+//   key          = "project_name"
+//   value        = var.project_name
+//   category     = "terraform"
+//   workspace_id = tfe_workspace.workspace_b.id
+// }
 
 
-resource "tfe_variable" "region" {
-  key          = "region"
-  value        = var.region
-  category     = "terraform"
-  workspace_id = tfe_workspace.workspace_b.id
-}
+// resource "tfe_variable" "region" {
+//   key          = "region"
+//   value        = var.region
+//   category     = "terraform"
+//   workspace_id = tfe_workspace.workspace_b.id
+// }
 
 
-resource "tfe_variable" "github_token" {
-  key          = "github_token"
-  value        = var.github_token
-  category     = "env"
-  sensitive    = true
-  workspace_id = tfe_workspace.workspace_b.id
-}
-  
+// resource "tfe_variable" "github_token" {
+//   key          = "github_token"
+//   value        = var.github_token
+//   category     = "env"
+//   sensitive    = true
+//   workspace_id = tfe_workspace.workspace_b.id
+// }
+
+// // resource "tfe_workspace" "workspace_c" {
+// //   name              = "workspace_c"
+// //   organization      = "db_test"
+// //   terraform_version = "0.14.5"
+// //   queue_all_runs    = false
+
+// //   vcs_repo {
+// //     identifier     = "LNPappas/repo_c"
+// //     branch         = "main"
+// //     oauth_token_id = "ot-2NQF7eb6F3E4s21g"
+// //   }
+// // }
+
+// // resource "tfe_variable" "c_tfvars_plan" {
+// //   key          = "TF_CLI_ARGS_plan"
+// //   value        = "terraform.auto.tfvars"
+// //   category     = "terraform"
+// //   workspace_id = tfe_workspace.workspace_c.id
+// // }
+
+// // resource "tfe_variable" "bucket_1" {
+// //   key          = "bucket_1"
+// //   value        = "${var.bucket_1}+${tfe_workspace.workspace_c.id}"
+// //   category     = "terraform"
+// //   workspace_id = tfe_workspace.workspace_c.id
+// // }
+
+
+// // resource "tfe_variable" "bucket_2" {
+// //   key          = "bucket_2"
+// //   value        = "${var.bucket_2}+${tfe_workspace.workspace_c.id}"
+// //   category     = "terraform"
+// //   workspace_id = tfe_workspace.workspace_c.id
+// // }
+
+
+// // resource "tfe_variable" "project_name" {
+// //   key          = "project_name"
+// //   value        = var.project_name
+// //   category     = "terraform"
+// //   workspace_id = tfe_workspace.workspace_c.id
+// // }
+
+
+// // resource "tfe_variable" "region" {
+// //   key          = "region"
+// //   value        = var.region
+// //   category     = "terraform"
+// //   workspace_id = tfe_workspace.workspace_c.id
+// // }
+
+
+// // resource "tfe_variable" "github_token" {
+// //   key          = "github_token"
+// //   value        = var.github_token
+// //   category     = "env"
+// //   sensitive    = true
+// //   workspace_id = tfe_workspace.workspace_c.id
+// // }
